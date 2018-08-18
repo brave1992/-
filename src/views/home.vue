cb967641031d310eacca743b9134f3a642087c8c
黑暗骑士 <18762807687@163.com>
2018-08-18 22:50:29 +0800

    首页整体布局

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    登录成功
+  <div class="home">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -16,8 +22,51 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .home {
+  height: 100%;
+  background-color: #E5E5E5;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+  }
+  .logo {
+    height:60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: white;
+  }
+  .toggle-btn {
+    padding: 0 10px;
+    margin-left: -20px;
+    font-size: 36px;
+    line-height: 60px;
+    color: white;
+    cursor: pointer;
+    &:hover {
+      background-color: #00635a;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome, {
+    color: white;
+  }
+}
 </style>
 
 
