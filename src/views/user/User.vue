15c7343014ad1368ab8adb2d6019840cdaaf41dd
黑暗骑士 <18762807687@163.com>
2018-08-26 16:54:10 +0800

    用户管理-用户列表-添加用户

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -15,7 +15,7 @@
         <el-input placeholder="请输入内容" class="search-input" v-model="query" @keydown.native.enter="initList">
           <el-button slot="append" icon="el-icon-search" @click="initList"></el-button>
         </el-input>
-        <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" plain @click="addDialogFormVisible=true">添加用户</el-button>
       </el-col>
     </el-row> 
     <el-table
@@ -61,10 +61,33 @@
         :total="total">
       </el-pagination>
     </div>
+
+      <!-- 添加用户对话框 -->
+    <el-dialog title="收货地址" :visible.sync="addDialogFormVisible">
+      <el-form :model="addForm" label-width="80px" :rules="rules" ref="addUserForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="phone"> 
+          <el-input v-model="addForm.phone" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDialogFormVisible=false">取 消</el-button>
+        <el-button type="primary" @click="addUserSubmit('addUserForm')">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 <script>
-import {getUserList,changeUserState} from '@/api'
+import {getUserList,changeUserState,addUser} from '@/api'
 export default {
    data() {
       return {
@@ -72,9 +95,33 @@ export default {
         query: '',
         total: 0,
         pageSize: 1,
-        pagenum: 1
+        pagenum: 1,
+        addDialogFormVisible: false,
+        addForm: {
+          username: '',
+          password: '',
+          email: '',
+          mobile: ''
+        },
+        // 添加用户的表单验证
+        rules: {
+          username: [
+            {required: true, message: '请输入用户名', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '请输入密码', trigger: 'blur'}
+          ],
+          email: [
+            {required: true, message: '请输入邮箱', trigger: 'blur'},
+            {type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change'}
+          ],
+          phone: [
+            {required: true, message: '电话不能为空', trigger: 'blur'}
+          ]
+        } 
       }
     },
+    
     // 在这些数据挂载到页面之前调用的
     created() {
       this.initList()
@@ -98,6 +145,7 @@ export default {
           this.total = res.data.total
         })
       },
+
       // 改变用户状态
       changeUserState(row) {
         console.log(row)
@@ -115,8 +163,27 @@ export default {
             })
           }
         })
-      }
+      },
 
+      // 添加用户  这段代码懵逼...
+      addUserSubmit(formName) {
+        this.$refs[formName].validate(valide => {
+          if(valide) {
+            // 执行用户添加方法
+            addUser(this.addForm).then(res => {
+              console.log(res)
+              if(res.meta.status === 201) {
+                this.$message({
+                  message: '创建用户成功',
+                  type: 'success'
+                })
+              } 
+              this.addDialogFormVisible = false 
+              this.initList()
+            })
+          }
+        })
+      }
     }
 }
 </script>
